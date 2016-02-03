0b611cbcaff688f6714891d77c3bb9ed539d021d
liubin <liubin365492@gmail.com>
2016-02-03 10:33:07 +0800

    2

--- YSGL/src/com/ysgl/dao/IUserDao.java
+++ YSGL/src/com/ysgl/dao/IUserDao.java
@@ -1,8 +1,18 @@
 package com.ysgl.dao;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ysgl.pojo.User;
 
 @Repository
+@Transactional
 public interface IUserDao {
-	
+	public void insertUser(User user);
+
+	public void updateUser(User user);
+
+	public void deleteUser(int id);
+
+	public User findById(int id);
 }
--- YSGL/src/com/ysgl/pojo/User.java
+++ YSGL/src/com/ysgl/pojo/User.java
@@ -3,6 +3,11 @@ package com.ysgl.pojo;
 public class User {
 	private int userId;
 	private String name;
+	private String account;
+	private String pwd;
+	private int dept;
+	
+	
 	
 	public int getUserId() {
 		return userId;
@@ -15,6 +20,24 @@ public class User {
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public int getDept() {
+		return dept;
+	}
+	public void setDept(int dept) {
+		this.dept = dept;
 	}
 	
 }
